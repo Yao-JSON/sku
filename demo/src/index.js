610e527f373c860125286eddbec0823854f186cc
姚嘉松 <yaojiasong@youzan.com>
2019-07-09 17:27:17 +0800

    fix: demo

--- demo/src/index.js
+++ demo/src/index.js
@@ -41,7 +41,63 @@ const sku = [
   },
 ];
 
+const value = [
+  {
+    "id": 10740,
+    "text": "颜色",
+    "value": 10740,
+    "leaf": [
+      {
+        "id": 3,
+        "text": "蓝色",
+        "value": 3,
+        "is_show": false
+      },
+      {
+        "id": 10,
+        "text": "红色",
+        "value": 10,
+        "is_show": false
+      },
+      {
+        "id": 30,
+        "text": "绿色",
+        "value": 30,
+        "is_show": false
+      }
+    ]
+  },
+  {
+    "id": 40732,
+    "text": "尺寸",
+    "value": 40732,
+    "leaf": [
+      {
+        "id": 30,
+        "text": "绿色",
+        "value": 30,
+        "is_show": false
+      },
+      {
+        "id": 3,
+        "text": "蓝色",
+        "value": 3,
+        "is_show": false
+      }
+    ]
+  }
+]
+
 class Simple extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: value
+    }
+  }
+
   fetchSKUTree() {
     return new Promise(resolve => {
       resolve(skuTree);
@@ -55,7 +111,10 @@ class Simple extends Component {
   }
 
   render() {
-    return <SKU onFetchGroup={this.fetchSKUTree} onFetchSKU={this.fetchSKU} />;
+    const { value } = this.state;
+    return <SKU disabled value={value} onChange={(res) => {
+      this.setState(value);
+    }} onFetchGroup={this.fetchSKUTree} onFetchSKU={this.fetchSKU} />;
   }
 }
 
